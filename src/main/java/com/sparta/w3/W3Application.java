df2878c8f60e6a689b3a7836e0cf50baa20b1b18
msk102948 <msk102948@gmail.com>
2022-03-23 10:10:10 +0900

    서버시간 수정/댓글내용 보이게 수정

--- src/main/java/com/sparta/w3/W3Application.java
+++ src/main/java/com/sparta/w3/W3Application.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class W3Application {
@@ -11,5 +14,7 @@ public class W3Application {
     public static void main(String[] args) {
         SpringApplication.run(W3Application.class, args);
     }
+    @PostConstruct
+    public void started(){ TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul")); }
 
 }
